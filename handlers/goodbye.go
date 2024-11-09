8acd3bee8ea2e15ff1f5fa73dfc17e106ec3892c
fabio pereira <fabiuhp@msn.com>
2024-11-09 19:34:28 -0300

    Add Goodbye handler and implement graceful shutdown in main application

--- handlers/goodbye.go
+++ handlers/goodbye.go
@@ -1,8 +1,18 @@
 package handlers
 
+import (
+	"log"
+	"net/http"
+)
+
 type Goodbye struct {
+	l *log.Logger
 }
 
-func NewGoodbye() *Goodbye {
+func NewGoodbye(l *log.Logger) *Goodbye {
 	return &Goodbye{}
+}
+
+func (g *Goodbye) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Bye!"))
 }
